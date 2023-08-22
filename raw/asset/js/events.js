fac6042e1919eb47d9a22da205fcdf05f09f99f7
Dap Moderator <123246207+thebanon@users.noreply.github.com>
2023-08-22 10:22:02 -0400

    Update Explorer Events
    Update Code Page
    Update Code Header UI
    Create Code File UI
    Update Explorer Navigator Events
    Create On Click CD Event Handler
    Create On Click CD Exit Handler

--- raw/asset/js/events.js
+++ raw/asset/js/events.js
@@ -1,6 +1,26 @@
 window.events = {};
 
 window.events.onclick = {}
+window.events.onclick.cd = function(dir) {
+    var href = null;
+    var pathname = window.location.pathname;
+    var paths = pathname.split('/').filter(o=>o.length > 0);
+    //console.log(pathname, paths);
+    if (paths[paths.length - 3] === "tree") {
+        if (paths[paths.length - 2] === "main") {
+            paths.splice(paths.length - 3, 3);
+        }
+    } else {
+        if (dir === "..") {
+            paths.pop();
+        }
+    }
+    if (paths) {
+        href = "/" + (paths.length > 0 ? paths.join("/") : "")
+        console.log(href, paths);
+        rout.er(href);
+    }
+}
 window.events.onclick.document = async function(event) {
     var target = event.target;
     var elem = target;
@@ -12,6 +32,15 @@ window.events.onclick.document = async function(event) {
         //console.log(47, href);
         browse.route(href);
     }
+}
+window.events.onclick.exit = function(event) {
+    var paths = window.location.pathname.split('/').filter(o=>o.length > 0);
+    paths[2] = "tree";
+    paths.pop();
+    //console.log(paths);
+    var href = "/" + (paths.length > 0 ? paths.join("/") : "");
+    //console.log(href);
+    rout.er(href);
 }
 
 window.events.onsubmit = {};
